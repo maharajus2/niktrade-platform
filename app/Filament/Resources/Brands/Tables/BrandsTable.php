f1dac07a12d56d971e64317fc2ffc2bf7f5779b4
Alexey Golovin <maharju111@gmail.com>
2026-06-03 16:38:26 +0300

    Configure brand resource

--- app/Filament/Resources/Brands/Tables/BrandsTable.php
+++ app/Filament/Resources/Brands/Tables/BrandsTable.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\Brands\Tables;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
 class BrandsTable
@@ -13,7 +15,19 @@ class BrandsTable
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('name')
+                    ->label('Название')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('country')
+                    ->label('Страна')
+                    ->searchable()
+                    ->sortable(),
+
+                IconColumn::make('is_active')
+                    ->label('Активен')
+                    ->boolean(),
             ])
             ->filters([
                 //

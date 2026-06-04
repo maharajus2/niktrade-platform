f142ddb73a0bda6e8013fc8a13deae79fbd842e0
Alexey Golovin <maharju111@gmail.com>
2026-06-04 16:34:01 +0300

    уменьшаю лого

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -31,7 +31,7 @@ class AdminPanelProvider extends PanelProvider
             //->brandLogo(fn () => view('filament.brand'))
             ->brandName('Никтрейд')
             ->brandLogo(asset('images/logont.png'))
-            ->brandLogoHeight('3rem')
+            ->brandLogoHeight('2rem')
             ->login()
             ->colors([
                 'primary' => Color::Sky,

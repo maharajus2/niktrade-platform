641cbb5febfc119c356d73b4c8232f48a20ebb29
Alexey Golovin <maharju111@gmail.com>
2026-06-04 15:15:18 +0300

    Custom filament brand

---
+++ resources/views/filament/brand.blade.php
@@ -0,0 +1,11 @@
+<div class="flex items-center gap-2">
+    <img
+        src="{{ asset('images/logo-icon.png') }}"
+        alt="Никтрейд"
+        style="height: 32px;"
+    >
+
+    <span class="text-lg font-bold">
+        НИКТРЕЙД
+    </span>
+</div>

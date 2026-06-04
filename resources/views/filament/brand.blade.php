76153e6d3e11dfb162c4fff7a6eef4aad7d67596
Alexey Golovin <maharju111@gmail.com>
2026-06-04 15:45:45 +0300

    Правим положение лого

--- resources/views/filament/brand.blade.php
+++ resources/views/filament/brand.blade.php
@@ -1,11 +1,20 @@
-<div class="flex items-center gap-2">
+<div class="flex items-center gap-3">
     <img
         src="{{ asset('images/logo-icon.png') }}"
         alt="Никтрейд"
-        style="height: 32px;"
+        style="height: 36px;"
     >
 
-    <span class="text-lg font-bold">
+    <span
+        class="font-bold"
+        style="
+            font-size: 22px;
+            line-height: 1;
+            position: relative;
+            top: -1px;
+            letter-spacing: 0.5px;
+        "
+    >
         НИКТРЕЙД
     </span>
 </div>

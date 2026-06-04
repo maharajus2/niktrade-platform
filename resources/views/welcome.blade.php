115433226e2fbc99223bdc68d66f293cfa285bea
Alexey Golovin <maharju111@gmail.com>
2026-06-04 17:53:58 +0300

    блюр 8

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,7 +27,7 @@
             background-size: cover;
             background-position: center;
 
-            filter: blur(12px);
+            filter: blur(8px);
             transform: scale(1.1);
 
             opacity: 0.6;

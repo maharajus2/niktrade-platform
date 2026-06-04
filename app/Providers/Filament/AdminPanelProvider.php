2430256d731cc0a1aa461cb575ee5605893e6ca7
Alexey Golovin <maharju111@gmail.com>
2026-06-04 16:13:34 +0300

    12rem

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,7 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
-            ->sidebarWidth('14rem')
+            ->sidebarWidth('12rem')
             //->brandLogo(fn () => view('filament.brand'))
             ->brandName('Никтрейд')
             ->brandLogo(asset('images/logont.png'))

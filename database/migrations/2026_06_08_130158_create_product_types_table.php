6fce061788b32f0456a600beae606e68d8807905
Alexey Golovin <maharju111@gmail.com>
2026-06-08 16:36:09 +0300

    попытка 4

--- database/migrations/2026_06_08_130158_create_product_types_table.php
+++ database/migrations/2026_06_08_130158_create_product_types_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('product_types', function (Blueprint $table) {
             $table->id();
+
+            $table->string('name');
+
+            $table->boolean('is_active')
+                ->default(true);
+
             $table->timestamps();
         });
     }

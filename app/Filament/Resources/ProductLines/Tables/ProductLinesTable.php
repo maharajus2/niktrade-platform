854467a6be461b9ee9d9dc992519340692989142
Alexey Golovin <maharju111@gmail.com>
2026-06-09 12:49:35 +0300

    Чиним линейки продуктов, чтобы они отображались в списке

--- app/Filament/Resources/ProductLines/Tables/ProductLinesTable.php
+++ app/Filament/Resources/ProductLines/Tables/ProductLinesTable.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\ProductLines\Tables;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
 class ProductLinesTable
@@ -13,7 +15,28 @@ class ProductLinesTable
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('brand.name')
+                    ->label('Бренд')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('name')
+                    ->label('Название линейки')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('slug')
+                    ->label('Slug')
+                    ->toggleable(),
+
+                IconColumn::make('is_active')
+                    ->label('Активна')
+                    ->boolean(),
+
+                TextColumn::make('created_at')
+                    ->label('Создана')
+                    ->dateTime('d.m.Y H:i')
+                    ->sortable(),
             ])
             ->filters([
                 //

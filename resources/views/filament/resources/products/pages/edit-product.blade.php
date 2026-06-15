3df302d3dbdf35c715a78878641587b992b60094
Alexey Golovin <maharju111@gmail.com>
2026-06-15 18:09:51 +0300

    12312

--- resources/views/filament/resources/products/pages/edit-product.blade.php
+++ resources/views/filament/resources/products/pages/edit-product.blade.php
@@ -3,23 +3,21 @@
         {{ $this->form }}
     </form>
 
-    <x-filament-panels::resources.relation-managers
-        :active-locale="property_exists($this, 'activeLocale') ? $this->activeLocale : null"
-        :active-manager="$this->activeRelationManager ?? null"
-        :content-tab-icon="method_exists($this, 'getContentTabIcon') ? $this->getContentTabIcon() : null"
-        :content-tab-label="method_exists($this, 'getContentTabLabel') ? $this->getContentTabLabel() : null"
-        :content-tab-position="method_exists($this, 'getContentTabPosition') ? $this->getContentTabPosition() : null"
-        :managers="$this->getRelationManagers()"
-        :owner-record="$record"
-        :page-class="static::class"
-    />
+    @livewire(
+        \App\Filament\Resources\Products\RelationManagers\ProductImagesRelationManager::class,
+        [
+            'ownerRecord' => $this->record,
+            'pageClass' => \App\Filament\Resources\Products\Pages\EditProduct::class,
+        ],
+        key('product-images-relation-manager-' . $this->record->getKey())
+    )
 
     <div class="flex items-center gap-3">
         <x-filament::button type="submit" form="product-edit-form">
             Сохранить
         </x-filament::button>
 
-        <x-filament::button color="gray" tag="a" :href="static::getResource()::getUrl('index')">
+        <x-filament::button color="gray" tag="a" :href="\App\Filament\Resources\Products\ProductResource::getUrl('index')">
             Отмена
         </x-filament::button>
     </div>

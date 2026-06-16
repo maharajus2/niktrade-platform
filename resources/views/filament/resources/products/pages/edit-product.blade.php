df080c074c1eadabbbe97790a9447607c01eab9d
Alexey Golovin <maharju111@gmail.com>
2026-06-17 01:15:26 +0300

    чиним правки карточки товара

--- resources/views/filament/resources/products/pages/edit-product.blade.php
+++ resources/views/filament/resources/products/pages/edit-product.blade.php
@@ -1,24 +1,19 @@
 <x-filament-panels::page>
-    <form id="product-edit-form" wire:submit="save">
+    <x-filament-panels::form wire:submit="save">
         {{ $this->form }}
-    </form>
 
-    @livewire(
-        \App\Filament\Resources\Products\RelationManagers\ProductImagesRelationManager::class,
-        [
-            'ownerRecord' => $this->record,
-            'pageClass' => \App\Filament\Resources\Products\Pages\EditProduct::class,
-        ],
-        key('product-images-relation-manager-' . $this->record->getKey())
-    )
+        @livewire(
+            \App\Filament\Resources\Products\RelationManagers\ProductImagesRelationManager::class,
+            [
+                'ownerRecord' => $this->record,
+                'pageClass' => \App\Filament\Resources\Products\Pages\EditProduct::class,
+            ],
+            key('product-images-relation-manager-' . $this->record->getKey())
+        )
 
-    <div class="flex items-center gap-3">
-        <x-filament::button type="submit" form="product-edit-form">
-            Сохранить
-        </x-filament::button>
-
-        <x-filament::button color="gray" tag="a" :href="\App\Filament\Resources\Products\ProductResource::getUrl('index')">
-            Отмена
-        </x-filament::button>
-    </div>
+        <x-filament-panels::form.actions
+            :actions="$this->getCachedFormActions()"
+            :full-width="$this->hasFullWidthFormActions()"
+        />
+    </x-filament-panels::form>
 </x-filament-panels::page>

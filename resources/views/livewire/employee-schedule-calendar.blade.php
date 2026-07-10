9e769949173d96f686b71bea2afa77a5f7e0039b
Alexey Golovin <maharju111@gmail.com>
2026-07-10 15:43:19 +0300

    Fix desktop calendar period picker layering

--- resources/views/livewire/employee-schedule-calendar.blade.php
+++ resources/views/livewire/employee-schedule-calendar.blade.php
@@ -215,7 +215,7 @@
                             await loadAsset('link[data-nt-employee-schedule-css]', () => {
                                 const link = document.createElement('link')
                                 link.rel = 'stylesheet'
-                                link.href = '{{ asset('css/employee-schedule-calendar.css') }}?v=20260710-mobile-safe-bottom-v4'
+                                link.href = '{{ asset('css/employee-schedule-calendar.css') }}?v=20260710-desktop-period-picker-v5'
                                 link.dataset.ntEmployeeScheduleCss = 'true'
 
                                 return link
@@ -223,7 +223,7 @@
 
                             await loadAsset('script[data-nt-employee-schedule-js]', () => {
                                 const script = document.createElement('script')
-                                script.src = '{{ asset('js/employee-schedule-calendar.js') }}?v=20260710-mobile-safe-bottom-v4'
+                                script.src = '{{ asset('js/employee-schedule-calendar.js') }}?v=20260710-desktop-period-picker-v5'
                                 script.dataset.ntEmployeeScheduleJs = 'true'
 
                                 return script

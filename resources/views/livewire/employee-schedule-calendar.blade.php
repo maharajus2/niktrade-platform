f97e7fd3653aa6ee81747a419d0313a8ae7f2f4c
Alexey Golovin <maharju111@gmail.com>
2026-07-10 12:04:31 +0300

    Prevent mobile calendar strip from covering picker

--- resources/views/livewire/employee-schedule-calendar.blade.php
+++ resources/views/livewire/employee-schedule-calendar.blade.php
@@ -215,7 +215,7 @@
                             await loadAsset('link[data-nt-employee-schedule-css]', () => {
                                 const link = document.createElement('link')
                                 link.rel = 'stylesheet'
-                                link.href = '{{ asset('css/employee-schedule-calendar.css') }}?v=20260710-mobile-picker-v2'
+                                link.href = '{{ asset('css/employee-schedule-calendar.css') }}?v=20260710-mobile-picker-v3'
                                 link.dataset.ntEmployeeScheduleCss = 'true'
 
                                 return link
@@ -223,7 +223,7 @@
 
                             await loadAsset('script[data-nt-employee-schedule-js]', () => {
                                 const script = document.createElement('script')
-                                script.src = '{{ asset('js/employee-schedule-calendar.js') }}?v=20260710-mobile-picker-v2'
+                                script.src = '{{ asset('js/employee-schedule-calendar.js') }}?v=20260710-mobile-picker-v3'
                                 script.dataset.ntEmployeeScheduleJs = 'true'
 
                                 return script
